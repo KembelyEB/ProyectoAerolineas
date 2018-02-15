396d063a188866b611dc1f311b2b9dec55b447f6
Kembely <kyespinoza@est.utn.ac.cr>
2018-02-15 09:17:19 -0600

    Creacion de clases datos .. Metodo de insertar modificar y eliminar de usuario

---
+++ ProyectoAerolineas/AerolineasDATOS/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineasDATOS
+{
+    public class Conexion
+    {
+
+        static NpgsqlConnection conexion;
+        static NpgsqlCommand cmd;
+
+        public  void ConexionDT()
+        {
+            string servidor = "localhost";
+            int puerto = 5432;
+            string usuario = "postgres";
+            string clave = "lanegra15";
+            string baseDatos = "Aerolineas";
+
+            string cadenaConexion = "Server=" + servidor + ";" + "Port=" + puerto + ";" + "User Id=" + usuario + ";" + "Password=" + clave + ";" + "Database=" + baseDatos;
+            conexion = new NpgsqlConnection(cadenaConexion);
+        }
+    }
+}
---
+++ ProyectoAerolineas/AerolineasDATOS/UsuarioDA.cs
@@ -0,0 +1,47 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineasDATOS
+{
+    public class UsuarioDA
+    {
+
+
+        Conexion ClsConexion = new Conexion();
+        static NpgsqlConnection conexion;
+        static NpgsqlCommand cmd;
+
+      
+
+        public void InsertarDatos(string cedula, string nombre, string contraseña )
+            {
+                ClsConexion.ConexionDT();
+                conexion.Open();
+                cmd = new NpgsqlCommand("INSERT INTO estudiante (cedula, nombre, contraseña) VALUES ('" + cedula + "', '" + nombre + "', '" + contraseña  + "')", conexion);
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+            }
+
+            public void ModificarDatos(string cedula, string nombre, string contraseña)
+            {
+            ClsConexion.ConexionDT();
+            conexion.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("UPDATE estudiante SET nombre = '" + nombre + "', edad = '" + contraseña  + "' WHERE cedula = '" + cedula + "'", conexion);
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+            }
+
+            public void EliminarDatos(int cedula)
+            {
+            ClsConexion.ConexionDT();
+            conexion.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("DELETE FROM estudiante WHERE cedula = '" + cedula + "'", conexion);
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+            }
+        }
+}
